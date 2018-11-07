fd804d8fb05f66a116d660c37851938ef2172dd3
Sultan0617 <44487198+Sultan0617@users.noreply.github.com>
2018-11-07 09:33:12 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+
+
 import logging
 import random
-from queue import Queue
-from threading import Thread
-from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, ParseMode
-from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Updater, Filters, CallbackQueryHandler
+#from queue import Queue
+#from threading import Thread
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from telegram.ext import CommandHandler, MessageHandler, Updater, Filters, CallbackQueryHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -12,15 +14,17 @@ TOKEN = '693266929:AAHv5cYEQmTI0kkClSWNK-CtRr7oqrXp3mI'
 
 
 def start(bot, update):
-    """Send a message when the command /start is issued."""
+    """Вывести сообщение, когда отправлена команда /start.
+    Обычно это приветственное сообщение"""
     """update.message.reply_text('Welcome to the Test Bot! I will reply you what you will write me.')"""
     bot.send_message(chat_id=update.message.chat_id,
                      text='<b>KAZGUU site</b>,<a href="http://kazguu.kz/ru/">KAZGUU</a>', parse_mode=ParseMode.HTML)
 
 
 def help(bot, update):
-    """Send a message when the command /help is issued."""
-    # update.message.reply_text('You can get any help here.')
+    """Вывести сообщение, когда отправлена команда /start.
+    Это может быть сообщение о том, что ваш бот может делать и список команд"""
+    update.message.reply_text('Тут вы можете получить любую помощь.')
 
     keyboardButtons = [[InlineKeyboardButton("Помощь", callback_data="1")],
                        [InlineKeyboardButton("Примеры", callback_data="2")],
@@ -32,88 +36,82 @@ def help(bot, update):
 def button(bot, update):
     query = update.callback_query
     if query.data == "1":
-        text = "You can choose any of the following actions: +, -, /, *"
+        text = "Вы можете использовать какое-либо из данных действий: +, -, /, *"
     elif query.data == "2":
         text = "3+4, 44-12, 43/2, 12*90"
     bot.editMessageText(text=text, chat_id=query.message.chat_id,
                         message_id=query.message.message_id)
 
 
-def ar(bot, update):
-    random_num = random.randint(1, 100)
-    num_true = True
-    try:
-        num = int(update.message.text)
-        while num_true == True:
-            if random_num == num:
+class arr:
+
+    def __init__(self, num, random_num=random.randint(1, 100)):
+        self.random_num = random_num
+        self.num = num
+
+    def checking(self, bot, update):
+        try:
+            if self.random_num == self.num:
                 ar = 'Вы выиграли!'
-                num_true = False
-            elif random_num > num:
+            elif self.random_num > self.num:
                 ar = 'Ваше число меньше моего. Попробуйте еще раз :)'
-            elif random_num < num:
+            elif self.random_num < self.num:
                 ar = 'Ваше число больше моего. Попробуйте еще раз :)'
-        # ar = int(num) * 370
-    except (NameError, SyntaxError, ValueError):
-        ar = "Введите целое число"
-    num_true = True
-    bot.send_message(chat_id=update.message.chat_id, text=ar)
+            # ar = int(num) * 370
+        except (NameError, SyntaxError, ValueError):
+            ar = "Введите целое число"
+        bot.send_message(chat_id=update.message.chat_id, text=ar)
 
 
-"""def echo(bot, update):
-    update.message.reply_text('You said:  ' + update.message.text)
-"""
+def guessing(bot, update, args):
+    #input_num = int(update.message.text)
+    input_num = int(args[0])
+    object_num = arr()
+    object_num.checking(input_num)
 
-"""def dolintenge(bot, update, args):
-    dollars = int(args)
-    tenge = dollars * 356
-    update.message.reply_text(text=tenge)
-"""
+
+def convert(bot, update, args):
+    try:
+        dollars = int(args[0])
+        tenge = dollars * 373
+    except (SyntaxError, ValueError):
+        tenge = "Введите целое число"
+    bot.send_message(chat_id=update.message.chat_id, text=tenge)
+
+
+#def echo(bot, update):
+#    update.message.reply_text('Вы ввели:  ' + update.message.text)
 
 
 def error(bot, update, error):
-    """Log Errors caused by Updates."""
+    """Запись всех ошибок вызванных Updates."""
     logger.warning('Update "%s" caused error "%s"' % (update, error))
 
 
-# Write your handlers here
+def main():
+    updater = Updater(TOKEN)  # Создаем EventHandler (обработчик событий) и передаем ему токен (ключ) бота.
+    #bot = updater.bot
+    dp = updater.dispatcher  # Объявление диспетчера, чтобы потом зарегистрировать handlers (обработчики)
+    dp.add_handler(CommandHandler("start", start))  # Отвечает на команду /start в Телеграм
+    dp.add_handler(CommandHandler("help", help))  # Отвечает на команду /help в Телеграм
+    dp.add_handler(CommandHandler("convert", convert, pass_args=True))
+    dp.add_handler(CallbackQueryHandler(button))
+    dp.add_handler(CommandHandler("guessing", guessing, pass_args=True))
 
+    # Для ответа бота на текстовые (не командные) сообщения.
+    #dp.add_handler(MessageHandler(Filters.text, echo))  # Бот отвечает тем сообщением, которое вы ему написали (эхо-бот)
+    # dp.add_handler(MessageHandler(Filters.text, guessing))
 
-def setup(webhook_url=None):
-    """If webhook_url is not passed, run with long-polling."""
-    logging.basicConfig(level=logging.WARNING)
-    if webhook_url:
-        bot = Bot(TOKEN)
-        update_queue = Queue()
-        dp = Dispatcher(bot, update_queue)
-    else:
-        updater = Updater(TOKEN)  # Create the EventHandler and pass it your bot's token.
-        bot = updater.bot
-        dp = updater.dispatcher  # Get the dispatcher to register handlers
-        dp.add_handler(CommandHandler("start", start))  # on /start command answer in Telegram
-        dp.add_handler(CommandHandler("help", help))  # on /help command answer in Telegram
-        """dp.add_handler(CommandHandler("dolintenge", dolintenge))"""
-        dp.add_handler(CallbackQueryHandler(button))
+    # Запись всех ошибок
+    dp.add_error_handler(error)
 
-        # on noncommand i.e message - echo the message on Telegram
-        """dp.add_handler(MessageHandler(Filters.text, echo))"""
-        dp.add_handler(MessageHandler(Filters.text, ar))
-
-        # log all errors
-        dp.add_error_handler(error)
-    # Add your handlers here
-    if webhook_url:
-        bot.set_webhook(webhook_url=webhook_url)
-        thread = Thread(target=dp.start, name='dispatcher')
-        thread.start()
-        return update_queue, bot
-    else:
-        bot.set_webhook()  # Delete webhook
-        updater.start_polling()  # Start the Bot
-        """Run the bot until you press Ctrl-C or the process receives SIGINT,
-        SIGTERM or SIGABRT. This should be used most of the time, since
-        start_polling() is non-blocking and will stop the bot gracefully."""
-        updater.idle()
+    updater.start_polling()  # Start the Bot
+    """Бот будет работать до тех пор пока вы не нажмете Ctrl-C
+     или процесс не получит SIGINT, SIGTERM или SIGABRT. 
+     Этот способ должен использоваться в большинстве случаев т.к. start_polling()
+      не блокирующий и остановит бота правильно."""
+    updater.idle()
 
 
 if __name__ == '__main__':
-    setup()
+    main()
